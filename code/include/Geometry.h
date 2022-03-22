3ec7131c967f9e44672433f588d8962ed45c1638
MarcVIc <marc.vicente@enti.cat>
2022-03-22 17:34:00 +0100

    Changed Math by GEometry , Errors Fixed

---
+++ code/include/Geometry.h
@@ -0,0 +1,25 @@
+#pragma once
+#pragma once
+
+#include <glm/glm.hpp>
+
+class Plane
+{
+public:
+    Plane();
+    ~Plane();
+
+    glm::vec3 planePoint;
+    glm::vec3 planeNormal;
+    float planeD;
+
+    void CheckColision(glm::vec3 pointPos, glm::vec3 pointVel);
+
+
+private:
+    Plane* plane;
+
+
+};
+
+
